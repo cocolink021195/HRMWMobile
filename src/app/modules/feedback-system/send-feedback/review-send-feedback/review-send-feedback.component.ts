b92f1b77c06302d51df8f213e9d37e4a09ae446d
lequyenbl <lequyenbl@gmail.com>
2020-06-02 16:09:10 +0700

    background

--- src/app/modules/feedback-system/send-feedback/review-send-feedback/review-send-feedback.component.ts
+++ src/app/modules/feedback-system/send-feedback/review-send-feedback/review-send-feedback.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as $ from 'jquery';
 
 @Component({
   selector: 'ons-page[app-review-send-feedback]',
@@ -8,7 +9,19 @@ export class ReviewSendFeedbackComponent implements OnInit {
   title = 'Thank you note - Xem'
   constructor() { }
 
-  ngOnInit() {
+  ngOnInit() {  
+    window.addEventListener('resize', function (event) {
+      var imgHeight = document.getElementById("cart-background");      
+      const clientHeight = imgHeight && imgHeight.clientHeight || 0;
+      if (clientHeight == 0) return;
+      $('.js-view-background').css('height', clientHeight / 3 + 'px');      
+    });
+  }
+  onImageLoad() {
+    const heightImg = $('#cart-background').height();    
+    if (heightImg > 0) {
+      $('.js-view-background').css('height', heightImg / 3 - 20 + 'px');
+    }
   }
 
 }
